7cd5b5b853593f0c7830f4494cf3d0d11b4755df
Altiplano6563 <vanessagusan@gmail.com>
2025-05-30 18:21:07 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FinalApp from './FinalApp';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+
+// Remova ou comente estas linhas:
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
 
 ReactDOM.render(
   <React.StrictMode>
