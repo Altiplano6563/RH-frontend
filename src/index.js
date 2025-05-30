661e2153ca2a7dc93a399ebef8fa53b9ec99ea52
Altiplano6563 <vanessagusan@gmail.com>
2025-05-29 21:20:58 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
+// Para React 18+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import SimpleAuthApp from './SimpleAuthApp';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SimpleAuthApp />
+  </React.StrictMode>
+);
+
+// OU para React 17 e anteriores
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AuthApp from './AuthApp';
+import SimpleAuthApp from './SimpleAuthApp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthApp />
+    <SimpleAuthApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
+*/
